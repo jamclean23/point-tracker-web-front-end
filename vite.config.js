84c856aef19562cf205c3f358ac7d4b6d5d96757
jamclean23 <jamclean23@gmail.com>
2024-09-04 12:42:19 -0400

    Fix vite.config.js to correctly build multiple pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 const root = resolve(__dirname, 'src');
+console.log(root);
 const outDir = resolve(__dirname, 'dist');
 
 // https://vitejs.dev/config/
@@ -12,11 +13,13 @@ export default defineConfig({
   build: {
     outDir,
     emptyOutDir: true,
-    input: {
-      // ADD PAGES HERE
-      password_reset: resolve(root, 'password_reset', 'index.html'),
-      admin_dashboard: resolve(root, 'admin_dashboard', 'index.html'),
-      admin_dashboard_login: resolve(root, 'admin_dashboard_login', 'index.html')
+    rollupOptions: {
+      input: {
+        // ADD PAGES HERE
+        password_reset: resolve(root, 'password_reset', 'index.html'),
+        admin_dashboard: resolve(root, 'admin_dashboard', 'index.html'),
+        admin_dashboard_login: resolve(root, 'admin_dashboard_login', 'index.html')
+      }
     }
   }
 })
